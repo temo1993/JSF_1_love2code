e94d6cb1cd2c0e45617e4a24a4ed0fd27dfa480b
Temo <temo.kiknavelidze@gmail.com>
2016-04-04 23:56:11 +0400

    ...drop down list using List to generate Countries...

--- src/bean/StudentTwo.java
+++ src/bean/StudentTwo.java
@@ -3,6 +3,9 @@ package bean;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @ManagedBean
 @ApplicationScoped
@@ -11,7 +14,26 @@ public class StudentTwo implements Serializable{
     private String lastName;
     private String country;
 
+    // List of countries for the drop-down list
+    private List<String> countryOptions;
+
     public StudentTwo() {
+        // Populate the list of countries
+        countryOptions = new ArrayList<>();
+        countryOptions.add("Brazil");
+        countryOptions.add("France");
+        countryOptions.add("Germany");
+        countryOptions.add("India");
+        countryOptions.add("Turkey");
+        countryOptions.add("United Kingdom");
+        countryOptions.add("United States");
+        countryOptions.add("Georgia");
+        Collections.sort(countryOptions);
+    }
+
+    // need to add a getter method for country options
+    public List<String> getCountryOptions() {
+        return countryOptions;
     }
 
     public String getFirstName() {
