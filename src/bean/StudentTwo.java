463a5349d4b8c752de7df2a2bd93ccbe4a94c7df
Temo <temo.kiknavelidze@gmail.com>
2016-04-04 23:23:47 +0400

    ...drop down list review...

---
+++ src/bean/StudentTwo.java
@@ -0,0 +1,40 @@
+package bean;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+
+@ManagedBean
+@ApplicationScoped
+public class StudentTwo implements Serializable{
+    private String firstName;
+    private String lastName;
+    private String country;
+
+    public StudentTwo() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
